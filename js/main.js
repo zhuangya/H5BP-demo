20e735fa4ce26c8ee69aa0682de8bb5b57915245
Zhuang Ya <zhuangya@gmail.com>
2013-08-02 04:26:03 +0800

    Just some fun with jquery and media query

--- js/main.js
+++ js/main.js
@@ -1,38 +1,22 @@
-var neoRender = function(id, local) {
-  local = local || mock;
-  var targetDom = document.getElementById(id);
-  var holderDom = document.getElementById('tpl-' + id);
-  var template = '';
+var neoRender = function(ids, local) {
+  $.each(ids, function(i) {
 
-  //if(!holderDom) throw new DomNotExistException(id);
+    var targetDom = document.getElementById(ids[i]);
+    var holderDom = document.getElementById('tpl-' + ids[i]);
+    var template = '';
 
-  //TODO: how to handle error?
-  try {
+    if(!holderDom) throw "There is no such element on the page";
     template = holderDom.innerHTML;
-  } catch(exception) {
-    console.log(exception);
-    throw new DomNotExistException(id);
-  }
 
-  targetDom.innerHTML = Mustache.render(template, local);
+    targetDom.innerHTML = Mustache.render(template, local);
+  });
 };
-
-function DomNotExistException(id) {
-  this.value = id;
-  this.message = " does not exist";
-  this.toString = function() {
-    return this.value + this.message;
-  };
-}
 
 $(function() {
   $.getJSON('./mock.json', function(mock) {
-    neoRender('header', mock);
-    neoRender('content', mock);
-
-    //TODO: when error occurs, report it, and keep going
-    neoRender('damn', mock);
-
-    neoRender('sidebar', mock);
+    neoRender(['header', 'content', 'favs'], mock);
+  });
+  $('#header').on('click', '#alertSign', function() {
+    $('#header').find('hgroup').toggle();
   });
 });
